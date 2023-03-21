65650a78afca97adc748ae021b9809279d4a30e2
Hosmel Quintana <hosmelq@gmail.com>
2023-03-20 23:05:46 -0600

    update deps

--- api/hook.rs
+++ api/hook.rs
@@ -1,45 +1,40 @@
-extern crate serde;
-extern crate serde_json;
-
-use http::StatusCode;
 use serde_json::{json, Value};
-use std::{env, error::Error};
-use vercel_lambda::{error::VercelError, lambda, IntoResponse, Request, Response};
+use std::env;
+use vercel_runtime::{run, Body, Error, Request, Response, StatusCode};
 
-fn handler(request: Request) -> Result<impl IntoResponse, VercelError> {
-    let payload: Value = serde_json::from_slice(&request.into_body()).unwrap();
-    let event = &payload["event"];
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    run(handler).await
+}
 
-    if event["type"] == "url_verification" {
-        let response = Response::builder()
+pub async fn handler(req: Request) -> Result<Response<Body>, Error> {
+    let payload: Value = serde_json::from_slice(&req.into_body()).unwrap();
+
+    if payload["type"] == "url_verification" {
+        return Ok(Response::builder()
             .status(StatusCode::OK)
             .header("Content-Type", "text/plain")
-            .body(event["challenge"].to_string())
-            .unwrap();
-
-        return Ok(response);
+            .body(Body::Text(payload["challenge"].to_string()))?);
     }
+
+    let event = &payload["event"];
 
     if event["type"] == "emoji_changed" && event["subtype"] == "add" {
         let emoji_name = event["name"].as_str().unwrap();
         let body = json!({
            "text": format!("New Emoji added. `:{}:` :{}:", emoji_name, emoji_name),
         });
-        let client = reqwest::blocking::Client::new();
-        let _ = client
+        let client = reqwest::Client::new();
+
+        client
             .post(env::var("SLACK_WEBHOOK_URL").unwrap())
             .json(&body)
-            .send();
+            .send()
+            .await?;
     }
 
-    let response = Response::builder()
+    return Ok(Response::builder()
         .status(StatusCode::OK)
-        .body(String::from(""))
-        .unwrap();
-
-    return Ok(response);
-}
-
-fn main() -> Result<(), Box<dyn Error>> {
-    Ok(lambda!(handler))
+        .header("Content-Type", "text/plain")
+        .body(Body::from(""))?);
 }
